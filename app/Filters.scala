2dc5e4a05f6a1eb349d42d58c5bf98cd503ae4a5
thuva4 <tharma.thuva@gmail.com>
2017-06-21 17:01:52 +0530

    create the tag inputs

--- app/Filters.scala
+++ app/Filters.scala
@@ -7,18 +7,18 @@ import play.filters.headers.SecurityHeadersFilter
 import play.filters.hosts.AllowedHostsFilter
 
 /**
- * Add the following filters by default to all projects
- * 
- * https://www.playframework.com/documentation/latest/ScalaCsrf 
- * https://www.playframework.com/documentation/latest/AllowedHostsFilter
- * https://www.playframework.com/documentation/latest/SecurityHeaders
- */
-class Filters @Inject() (
-  csrfFilter: CSRFFilter,
-  allowedHostsFilter: AllowedHostsFilter,
-  securityHeadersFilter: SecurityHeadersFilter
-) extends DefaultHttpFilters(
-  csrfFilter, 
-  allowedHostsFilter, 
+  * Add the following filters by default to all projects
+  *
+  * https://www.playframework.com/documentation/latest/ScalaCsrf
+  * https://www.playframework.com/documentation/latest/AllowedHostsFilter
+  * https://www.playframework.com/documentation/latest/SecurityHeaders
+  */
+class Filters @Inject()(
+                         csrfFilter: CSRFFilter,
+                         allowedHostsFilter: AllowedHostsFilter,
+                         securityHeadersFilter: SecurityHeadersFilter
+                       ) extends DefaultHttpFilters(
+  csrfFilter,
+  allowedHostsFilter,
   securityHeadersFilter
 )
