7ff7cd88d85c00befeaac10d9d77a37b1ef8da94
thuva4 <tharma.thuva@gmail.com>
2017-06-21 15:27:02 +0530

    user model implemented

--- app/models/User.scala
+++ app/models/User.scala
@@ -14,7 +14,6 @@ import com.mohiva.play.silhouette.api.{ Identity, LoginInfo }
   * @param fullName Maybe the full name of the authenticated user.
   * @param email Maybe the email of the authenticated provider.
   * @param avatarURL Maybe the avatar URL of the authenticated provider.
-  * @param activated Indicates that the user has activated its registration.
   */
 case class User(
                  userID: UUID,
@@ -23,8 +22,7 @@ case class User(
                  lastName: Option[String],
                  fullName: Option[String],
                  email: Option[String],
-                 avatarURL: Option[String],
-                 activated: Boolean) extends Identity {
+                 avatarURL: Option[String]) extends Identity {
 
   /**
     * Tries to construct a name.
