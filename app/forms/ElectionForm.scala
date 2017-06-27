aa105d356f131feec5a062e00aa567fc572fe911
thuva4 <tharma.thuva@gmail.com>
2017-06-27 17:58:08 +0530

    fixes & typos

--- app/forms/ElectionForm.scala
+++ app/forms/ElectionForm.scala
@@ -15,7 +15,7 @@ object ElectionForm {
     mapping(
       "Name" -> nonEmptyText, "Description" -> nonEmptyText, "Creator name" -> nonEmptyText,
       "Creator email" -> email, "Starting Date" -> date, "Ending Date" -> date, "Realtime Result" -> boolean,
-      "Voting Algo" -> nonEmptyText, "Canditates" -> list(text), "Voting Preference" -> boolean,
+      "Voting Algo" -> nonEmptyText, "Candidates" -> nonEmptyText, "Voting Preference" -> boolean,
       "Invite Voters" -> boolean
     )(Election.apply)(Election.unapply)
   )
