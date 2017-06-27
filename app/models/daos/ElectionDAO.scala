fce52756846aa8ef07a33c5a50807f5210409343
thuva4 <tharma.thuva@gmail.com>
2017-06-27 17:57:09 +0530

    created the data access layer for election to communicate with the database

---
+++ app/models/daos/ElectionDAO.scala
@@ -0,0 +1,23 @@
+package models.daos
+
+import java.util.UUID
+
+import models.Election
+
+import scala.concurrent.Future
+
+/**
+ * Give access to the Election object.
+ */
+
+trait ElectionDAO {
+
+
+  /**
+   * Saves a Election.
+   *
+   * @param election The election to save.
+   * @return The saved election.
+   */
+  def save(election: Election): Future[Election]
+}
