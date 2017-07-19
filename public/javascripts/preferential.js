22e0c8a51bb51fee76f356c2a08634471d06ff8c
thuva4 <tharma.thuva@gmail.com>
2017-07-19 09:46:20 +0530

    edit the preferential ballot interface

--- public/javascripts/preferential.js
+++ public/javascripts/preferential.js
@@ -2,19 +2,20 @@ $( function() {
     $( "#sortable" ).sortable();
     $( "#sortable" ).disableSelection();
     $("#candidateDiv").on("click", '[id^="AAA_"].add',function() {
-       $(".ballotDiv").append( '<li class="ui-state-default preferential borderless" id ="'+ this.id +'">'+ this.value +'</li>' );
-       $( this ).replaceWith( '<button id="' + this.id + '" type="button" class="btn btn-danger btn-sm remove vote" value= "'+this.value +' ">remove</button>');
+    $(".ballotDiv").append( '<li class="ui-state-default preferential borderless" id ="'+ this.id +'">'+ '<button id= "' + this.id + '" type="button" class = "btn-danger remove" value= "'+this.value +' "><span class="glyphicon glyphicon-remove"></span></button>'+this.value +'</li>' );
+    $(this).parent().remove();
+
        return false;
    }),
-  $("#candidateDiv").on("click" , "button.remove" , function () {
+  $(".ballotDiv").on("click" , "button.remove" , function () {
           $(".ballotDiv").children().remove("#"+this.id);
-          $( this ).replaceWith('<button id="' + this.id+ '" type="button" class="btn btn-default btn-sm add vote" value= "'+this.value +' ">Add</button>');
+          $("#candidateDiv").children("ul").append('<li class="r0_0"><button id='+ this.id +' value='+ this.value+ 'type="button" class="btn btn-default btn-sm add vote">Add</button><span>'+this.value+'</span></li>')
     })
 
       $(".text-center.bottomm10").on("click", '#enter-vote-btn',function() {
         var str = "";
           $(".ballotDiv").children().each(function () {
-                  str+=this.innerHTML+">";
+                  str+=this.children[0].value+">";
                   str = str.replace(/\s/g, '');
                 });
           $("#ballotinput").attr("value", str.substring(0, str.length-1));
