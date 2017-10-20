2a2d11c0a8050edd6bce1f4c97a85a06722629f4
thuva4 <tharma.thuva@gmail.com>
2017-10-20 11:42:55 +0530

    # GET         /account/email/:email/:id         controllers.ElectionController.send(email: String,id)

--- app/controllers/SignInController.scala
+++ app/controllers/SignInController.scala
@@ -41,27 +41,4 @@ class SignInController @Inject()(
   def view = silhouette.UnsecuredAction.async { implicit request =>
     Future.successful(Ok(views.html.signIn(socialProviderRegistry)))
   }
-
-  def submit(email: String , id: String) = silhouette.UnsecuredAction.async { implicit request =>
-    val password = PassCodeGenerator.encrypt(email,id)
-    val credentials = Credentials(email, password)
-    credentialsProvider.authenticate(credentials).flatMap { loginInfo =>
-      val result = Redirect(routes.ElectionController.viewElectionSecured(id))
-      userService.retrieve(loginInfo).flatMap {
-        case Some(user) =>
-          silhouette.env.authenticatorService.create(loginInfo).map {
-            case authenticator => authenticator
-          }.flatMap { authenticator =>
-            silhouette.env.eventBus.publish(LoginEvent(user, request))
-            silhouette.env.authenticatorService.init(authenticator).flatMap { v =>
-              silhouette.env.authenticatorService.embed(v, result)
-            }
-          }
-        case None => Future.failed(new IdentityNotFoundException("Couldn't find user"))
-      }
-    }.recover {
-      case e: ProviderException =>
-        Redirect(routes.SignInController.view()).flashing("error" -> Messages("invalid.credentials"))
-    }
-  }
 }
